2408f2ce8be9b4fe176a6f34aafe5cadbe219c61
George Watson <gigolo@hotmail.co.uk>
2024-07-06 13:41:38 +0100

    save state

--- src/test.lisp
+++ src/test.lisp
@@ -1,9 +1,23 @@
 ;;;; test.lisp
 
-(defpackage :cl-sokol
-  (:use #:cl #:autowrap))
+(defpackage :cl-sokol-app
+  (:use #:cl)
+  (:nicknames :%sapp))
 
-(in-package :cl-sokol)
+(in-package :cl-sokol-app)
+
+(pushnew (asdf:system-relative-pathname :cl-sokol-app #p"build/")
+         cffi:*foreign-library-directories*
+         :test #'equal)
+
+(cffi:define-foreign-library sokol-sapp
+  (:darwin "libsokol_app.dylib")
+  (:unix "libsokol_app.so")
+  (:windows "libsokol_app.dll")
+  (t (:default "libsokol_app")))
+
+(unless (cffi:foreign-library-loaded-p 'sokol-sapp)
+  (cffi:use-foreign-library sokol-sapp))
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (defun cl-sokol::translate-to-lisp (name)
@@ -14,12 +28,8 @@
          name
          (subseq name 5)))))
 
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (cffi-sys:%load-foreign-library
-   :libtest (merge-pathnames "libsokol_app.dylib" (asdf-path 'cl-sokol))))
-
-(c-include '(cl-sokol "sokol/sokol_app.h")
-           :spec-path '(cl-sokol)
-           :exclude-definitions ("^_(?!SAPP)")
-           :include-definitions ("__darwin_intptr_t")
+(autowrap:c-include '(cl-sokol "sokol/sokol_app.h")
+                    :spec-path '(cl-sokol)
+                    :exclude-definitions ("^_(?!SAPP)")
+                    :include-definitions ("__darwin_intptr_t")
            :c-to-lisp-function #'translate-to-lisp)
